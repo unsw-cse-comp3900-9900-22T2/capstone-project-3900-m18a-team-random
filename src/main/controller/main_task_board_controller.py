4f60238513d85f892b8616a5e43890f34449e2b4
BarryLzc <successzhao2000@sina.com>
2022-06-22 22:03:45 -0400

    success add task through http request

--- src/main/controller/main_task_board_controller.py
+++ src/main/controller/main_task_board_controller.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route('/add-task',methods=['POST'])
 def add_task():
-    '''
+    
     data = request.get_json()
 
     task_name = data['taskname']
@@ -21,10 +21,13 @@ def add_task():
     task_status = data['status']
     task_priority = data['priority']
     task_due_date = data['due_date']
-    '''
 
-    return json.dumps(main_task_board_service.add_task('task12', 'barry', 'finish', 'high', '2022-07-01'))
+    result = main_task_board_service.add_task(task_name, person_name, task_status, task_priority, task_due_date)
 
+    if(result == False):
+        return json.dumps('Fail to  add task in task management')
+    
+    return json.dumps('Success to  add task in task management')
 
 if __name__ == '__main__':
     # set port number to 8080, threaded = True'
--- src/main/service/main_task_board_service.py
+++ src/main/service/main_task_board_service.py
@@ -6,7 +6,6 @@ import main_task_board
 def add_task(task_name, person_name, task_status, task_priority, task_due_date):
     if (main_task_board.search_task(task_name)):
         print("The task was already inserted in task board")
-        return 
+        return False
     main_task_board.add_task(task_name, person_name, task_status, task_priority, task_due_date)
-
-#add_task("task9", "barry", "undo", "high", "2022-7-01")
+    return True
