65ff9472291789732aea3ef5edb50251ff4c1bd2
Lubuntu <428266031@qq.com>
2022-07-12 12:30:35 -0400

    adding few setters for Task object

--- backend/teams/models.py
+++ backend/teams/models.py
@@ -69,7 +69,22 @@ class Task(db.Model):
     
     def __repr__(self):
         return f"Task('{self.title}','{self.status}','{self.priority}'"
+    
+    def set_description(self, description):
+        self.description = description
 
+    def set_status(self, status):
+        self.status = status
+    
+    def set_priority(self, priority):
+        self.priority = priority
+
+    def set_assignee_email(self, assignee_email):
+        self.assignee_email = assignee_email
+
+    def set_due_date(self, due_date):
+        self.due_date = due_date
+    
 class Comment(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     parent_id = db.Column(db.Integer)
