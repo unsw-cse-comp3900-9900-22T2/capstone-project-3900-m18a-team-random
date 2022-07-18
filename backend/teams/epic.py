a8b0c5ebae15618805bd95b27193da40a6e07c58
BarryLzc <successzhao2000@sina.com>
2022-07-17 23:19:56 -0400

    test for main

--- backend/teams/epic.py
+++ backend/teams/epic.py
@@ -21,7 +21,7 @@ def epic_create(token, epic, team_name):
     db.session.add(epic)
     db.session.commit()
 
-    return epic_list
+    return {"epic_id":epic.id}
 
 def get_team_from_team_name(team_name):
     if team_name not in get_active_teams():
